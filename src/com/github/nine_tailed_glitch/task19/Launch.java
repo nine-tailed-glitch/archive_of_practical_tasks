17c31bc205af9e14e0315e946e0f4587ffc016a1
Glitch <ntg-mailbox@internet.ru>
2022-11-01 21:16:48 +0300

    Nineteenth task (1159)

---
+++ src/com/github/nine_tailed_glitch/task19/Launch.java
@@ -0,0 +1,30 @@
+package com.github.nine_tailed_glitch.task19;
+
+import com.github.nine_tailed_glitch.general.AbstractLaunch;
+
+import java.util.Scanner;
+
+public class Launch extends AbstractLaunch {
+    @Override
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            if (isSimple(i))
+            {
+                System.out.printf("%d ", i);
+            }
+        }
+    }
+
+    private boolean isSimple(int n) {
+        if (n < 2)
+            return false;
+        double sqrt = Math.sqrt(n);
+        for (int i = 2; i <= sqrt; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
