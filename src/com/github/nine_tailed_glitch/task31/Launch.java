4c2427e3d884d528ac49a24cfb26908e1e3c5cd6
Glitch <ntg-mailbox@internet.ru>
2022-11-12 20:57:25 +0300

    Task 31 (1146): printRectangle (changes), new methods: printFourIsoscelesRightTriangles, printLine

--- src/com/github/nine_tailed_glitch/task31/Launch.java
+++ src/com/github/nine_tailed_glitch/task31/Launch.java
@@ -5,18 +5,63 @@ import com.github.nine_tailed_glitch.general.AbstractLaunch;
 public class Launch extends AbstractLaunch {
     @Override
     public void start() {
-        printRectangle();
+//        printRectangle(7, 3);
+//        printFourIsoscelesRightTriangles(15);
     }
 
     // прямоугольник
-    private void printRectangle() {
-        int length = 3;
-        int width = 7;
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < width; j++) {
-                System.out.print("\u002B\u0020");   // "+[space]" in hex
+    private void printRectangle(int w, int h) {
+        for (int i = 0; i < h; i++) {
+            printLine(w);
+        }
+        System.out.println();
+    }
+
+    // четыре равнобедренных прямоугольных треугольника
+    private void printFourIsoscelesRightTriangles(final int leg) {
+        // угол слева вверху
+        int tmpLeg = leg;
+        while (tmpLeg > 0) {
+            printLine(tmpLeg);
+            tmpLeg--;
+        }
+        System.out.println();
+
+        // угол справа снизу
+        tmpLeg = 1;
+        while (tmpLeg <= leg) {
+            printLine(tmpLeg);
+            tmpLeg++;
+        }
+        System.out.println();
+
+        // угол справа вверху
+        tmpLeg = leg;
+        while (tmpLeg > 0) {
+            for (int i = 0; i < leg - tmpLeg; i++) {
+                System.out.print("\u0020\u0020"); // "[space][space]" in hex
             }
-            System.out.println();
+            printLine(tmpLeg);
+            tmpLeg--;
+        }
+        System.out.println();
+
+        // угол слева снизу
+        tmpLeg = 1;
+        while (tmpLeg <= leg) {
+            for (int i = 0; i < leg - tmpLeg; i++) {
+                System.out.print("\u0020\u0020"); // "[space][space]" in hex
+            }
+            printLine(tmpLeg);
+            tmpLeg++;
+        }
+        System.out.println();
+    }
+
+    // напечатать указанное количество символов (разделенных пробелами)
+    private void printLine(int countSymbols) {
+        for (int i = 0; i < countSymbols; i++) {
+            System.out.print("\u002B\u0020"); // "+[space]" in hex
         }
         System.out.println();
     }
